22edb405742c6a4181494ab398fc1ff4945b3f3d
fawzy <111665714+Mohamed-fawzyy@users.noreply.github.com>
2024-09-25 12:32:21 +0300

    Time: 63 ms (43.42%), Space: 48.9 MB (33.98%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+function fib(n) {
+    if (n == 0 || n == 1) {
+        return n
+    }
+
+    let result = fib(n - 1) + fib(n - 2)
+    return result
+}

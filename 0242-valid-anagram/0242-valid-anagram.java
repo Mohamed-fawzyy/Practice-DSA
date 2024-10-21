d24b37445711db2c9eec44b54e3064df2e350a48
fawzy <111665714+Mohamed-fawzyy@users.noreply.github.com>
2024-10-21 16:17:33 +0300

    Time: 3 ms (90.15%), Space: 44.4 MB (46.87%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,21 +2,13 @@ class Solution {
     public boolean isAnagram(String s, String t) {
 
         if (s.length() != t.length()) return false;
-        
-        HashMap<Character, Integer> charMapCount = new HashMap<>();
 
-        for (char ch : s.toCharArray()){
-            charMapCount.put(ch, charMapCount.getOrDefault(ch, 0) + 1);
-        }
-        
-        for (char ch : t.toCharArray()){
-            
-            if(!charMapCount.containsKey(ch)) return false;
+        char[] arrS = s.toCharArray();
+        char[] arrT = t.toCharArray();
 
-            charMapCount.put(ch, charMapCount.get(ch) - 1);
-            if(charMapCount.get(ch) < 0) return false;
-        }
+        Arrays.sort(arrS);
+        Arrays.sort(arrT);
 
-        return true;
+        return Arrays.equals(arrS, arrT);
     }
 }

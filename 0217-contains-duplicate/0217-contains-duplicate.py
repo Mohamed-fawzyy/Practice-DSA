3e8d19358bca43faf3bc23468133869e57f45b00
fawzy <fwzymohamed90@gmail.com>
2025-03-01 23:55:18 +0200

    Empty commit for master branch

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        repeated = set()
-        for element in nums:
-            if element in repeated:
-                return True
-            repeated.add(element)
-        return False        

0ef82d90b98d36d8800308b7209db453169b650d
fawzy <111665714+Mohamed-fawzyy@users.noreply.github.com>
2025-04-15 10:00:14 +0200

    Time: 0 ms (100%), Space: 45 MB (11.9%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+        ListNode slwPtr = head;
+        ListNode fstPtr = head;
+
+        while (fstPtr != null && fstPtr.next != null){
+
+            slwPtr = slwPtr.next;
+            fstPtr = fstPtr.next.next;
+
+            if (slwPtr == fstPtr)
+                return true;
+        }
+
+        return false;
+    }
+}
